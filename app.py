604456ff6ba467742a881e8bd95064bfc706cf19
nakulrudrawr <nakulrudrawar7@gmail.com>
2026-06-16 07:36:33 +0530

    Remove commented-out code and streamline Flask application setup

--- app.py
+++ app.py
@@ -1,11 +1,3 @@
-""" from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-def hello_world():  
-    return 'Application is running successfully!'
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)  """
-
 from flask import Flask
 import os
 app = Flask(__name__)
