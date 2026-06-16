0c625236d686363501f96eacdd0223933ca44fce
nakulrudrawr <nakulrudrawar7@gmail.com>
2026-06-16 16:49:09 +0530

    updated

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_geek():
     return 'successfully deployed python application through jenkins!!!!!!!!!, added webhook'
+@app.route('/hi')
+def hell():
+    return '<h1>Hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii from Flask & Docker</h1>'
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
